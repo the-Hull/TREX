bc10111edc4fe559c1c7473da77e0728a1305694
Alex Hurley <the-Hull@users.noreply.github.com>
2021-10-14 19:40:29 +0000

    improved comments for predictions

--- R/allometry_pred.R
+++ R/allometry_pred.R
@@ -22,18 +22,14 @@ predict_allometry <- function(input_dbh_cm,
 
     `%nin%` <-  Negate(`%in%`)
 
-
     denv <- new.env()
     utils::data("allometry.data", envir = denv)
     utils::data("allometry.models", envir = denv)
 
-
-    allometry.data <- denv$allometry.data
+        allometry.data <- denv$allometry.data
     allometry.models <- denv$allometry.models
 
     rm(denv)
-
-
 
     if(any(parameter %nin% names(allometry.models))){
         stop("Please choose from the existing outputs: Bark_cm, Sap_cm, Saparea_cm2.")
@@ -54,48 +50,35 @@ predict_allometry <- function(input_dbh_cm,
         species <- unique(allometry.data$Species)
     }
 
-
-
+    # make named list for every parameter - setNames(parameter_predictions, parameter_names)
     predictions <- setNames(
 
         # parameter looping
         lapply(parameter,
                           function(parm){
 
-                              # species looping
+                              # make named list for every species - setNames(species_predictions, species_names)
                               spec_preds <- setNames(
+                              # species looping
                                   lapply(species,
                                          function(spec){
-
 
                                              preds <- predict(allometry.models[[parm]][['fit']][allometry.models[[parm]][['Species']] %in% spec][[1]],
                                                      newdat = data.frame(DBH_cm = input_dbh_cm),
                                                      ...)
-
                                              preds <- data.frame(value = preds)
 
                                          }),
-
-
                               species)
-
                               spec_preds <- dplyr::bind_rows(spec_preds, .id = "species")
-
-
-
-
-
                           }),
         parameter)
 
     predictions <- dplyr::bind_rows(predictions, .id = "parameter")
     rownames(predictions) <- NULL
 
-
     return(predictions)
-
 }
 
 
-predict_allometry(input_dbh_cm = 10)
 
